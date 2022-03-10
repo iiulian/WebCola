fff6d0556c1a76eb4561e7cb95b9d8ea729f2b28
Iulian Ilea <iulian.ilea@gmail.com>
2022-03-10 17:20:49 +0200

    Only the webcola code.

--- rollup.dts.config.js
+++ rollup.dts.config.js
@@ -1,13 +1,13 @@
 import dts from "rollup-plugin-dts";
 
 export default [
-  {
-    input: "./tmp/index.d.ts",
-    output: [{
-      file: "dist/cola.d.ts",
-      format: "esm",
-      banner: 'export as namespace cola;'
-    }],
-    plugins: [dts()],
-  },
+	{
+		input: "./tmp/index.d.ts",
+		output: [{
+			file: "dist/index.d.ts",
+			format: "esm",
+			banner: 'export as namespace cola;'
+		}],
+		plugins: [dts()],
+	},
 ];
